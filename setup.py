9833d9fec9315824201a122684ab065374869c61
Torty Sivill <tortysivill@Tortys-MacBook-Pro.local>
2024-10-14 11:37:33 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -10,21 +10,21 @@ def read_requirements(file_path):
 requirements = read_requirements('requirements.txt')
 
 setup(
-    name='decima2',
+    name="decima2",
     version="0.1.1",
     packages=find_packages(),
     install_requires=requirements,
     # Other metadata options
-    author='Torty Sivill',
-    author_email='tortysivill@decima2.co.uk',
-    description='Evaluation Toolkit for Machine Learning Models',
+    author="Torty Sivill",
+    author_email="torty.sivill@decima2.co.uk",
+    description="Evaluation Toolkit for Machine Learning Models",
     long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/Decima2/Decima2Toolkit',
+    long_description_content_type="text/markdown",
+    url="https://github.com/Decima2/Decima2Toolkit",
     classifiers=[
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",  # Corrected classifier
-        'Operating System :: OS Independent',
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6, <=3.12.7',
+    python_requires=">=3.6, <=3.12.7",
 )
